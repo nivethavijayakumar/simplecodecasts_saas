f158c975e179835ed7d4cd592cd1d3efd071a667
Nivetha Vijayakumar <nivetha_vijay@rocketmail.com>
2018-01-08 16:49:11 +0000

    Added Contact Mailer functionality

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,12 @@
+class ContactMailer < ActiveMailer::Base
+    default to: 'nivetha_vijay@rocketmail.com'
+    
+    def contact_email(name, email, body)
+        
+        @name = name
+        @email = email
+        @body = body
+        
+        mail(from: email, subject: 'Contact form Message')
+    end
+end
