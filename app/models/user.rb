a2bbab31a8a889ecd3c1c34f4af6acd49070a6d2
Nivetha Vijayakumar <nivetha_vijay@rocketmail.com>
2018-01-15 08:11:45 +0000

    Added profile model and table

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   belongs_to :plan
+  has_one :profile
   attr_accessor :stripe_card_token
   #If Pro user passes validations (email, password, etc )
   #then call Stripe and tell Stripe to set up a subscription
