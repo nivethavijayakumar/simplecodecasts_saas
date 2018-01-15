42d1b7b77e3fa54e682542b9710fc3b10925816d
Nivetha Vijayakumar <nivetha_vijay@rocketmail.com>
2018-01-15 11:33:42 +0000

    Added profiles form and create action

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: 'users/registrations'}
   resources :users do 
-    resources :profiles
+    resource :profile
   end
   resources :contacts
   get '/about' => 'pages#about'
